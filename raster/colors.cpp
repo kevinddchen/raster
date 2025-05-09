85cf988591d4b354c5ef0e2b4c4a072bcc786ccd
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2025-05-08 22:36:28 -0700

    Add gamma correction

---
+++ raster/colors.cpp
@@ -0,0 +1,95 @@
+#include <raster/colors.hpp>
+
+#include <ncurses.h>
+
+#include <algorithm>
+
+
+namespace
+{
+
+// Number of color levels
+constexpr int NUM_LEVELS = 6;
+// Color levels
+constexpr std::array<short, NUM_LEVELS> LEVELS = {0, 200, 400, 600, 800, 1000};
+
+// Offset to avoid overwriting ncurses default colors
+constexpr short COLOR_ENCODING_OFFSET = 8;
+// Offset to avoid overwriting ncurses default color pair
+constexpr short PAIR_ENCODING_OFFSET = 1;
+
+/**
+ * Convert color value in [0, 1] to the corresponding level, i.e. index of `LEVELS`.
+ */
+int color_to_level(float color)
+{
+    return std::clamp(static_cast<int>(std::floor(color * NUM_LEVELS)), 0, NUM_LEVELS - 1);
+}
+
+/**
+ * Convert a single sRGB value to linear.
+ */
+float srgb_to_linear(float value)
+{
+    if (value <= 0.04045) {
+        return value / 12.92;
+    } else {
+        return std::pow((value + 0.055) / 1.055, 2.4);
+    }
+}
+
+/**
+ * Convert a single linear value to sRGB.
+ */
+float linear_to_srgb(float value)
+{
+    if (value <= 0.0031308) {
+        return value * 12.92;
+    } else {
+        return std::pow(value, 1 / 2.4) * 1.055 - 0.055;
+    }
+}
+
+}  // namespace
+
+
+namespace raster
+{
+
+void init_colors()
+{
+    start_color();
+
+    // define colors
+    for (int r = 0; r < NUM_LEVELS; ++r) {
+        for (int g = 0; g < NUM_LEVELS; ++g) {
+            for (int b = 0; b < NUM_LEVELS; ++b) {
+                const short encoding = (r * NUM_LEVELS + g) * NUM_LEVELS + b;
+                const short color_encoding = encoding + COLOR_ENCODING_OFFSET;
+                const short pair_encoding = encoding + PAIR_ENCODING_OFFSET;
+                init_color(color_encoding, LEVELS[r], LEVELS[g], LEVELS[b]);
+                init_pair(pair_encoding, color_encoding, color_encoding);
+            }
+        }
+    }
+}
+
+short rgb_to_color_pair(const Eigen::Vector3f& color)
+{
+    const int r = color_to_level(color(0));
+    const int g = color_to_level(color(1));
+    const int b = color_to_level(color(2));
+    return (r * NUM_LEVELS + g) * NUM_LEVELS + b + PAIR_ENCODING_OFFSET;
+}
+
+Eigen::Vector3f srgb_to_linear(const Eigen::Vector3f& srgb)
+{
+    return {::srgb_to_linear(srgb(0)), ::srgb_to_linear(srgb(1)), ::srgb_to_linear(srgb(2))};
+}
+
+Eigen::Vector3f linear_to_srgb(const Eigen::Vector3f& linear)
+{
+    return {::linear_to_srgb(linear(0)), ::linear_to_srgb(linear(1)), ::linear_to_srgb(linear(2))};
+}
+
+}  // namespace raster
--- raster/colors.hpp
+++ raster/colors.hpp
@@ -1,25 +1,6 @@
 #pragma once
 
-#include <ncurses.h>
 #include <Eigen/Dense>
-
-#include <algorithm>
-
-
-namespace
-{
-
-// Number of color levels
-constexpr int NUM_LEVELS = 6;
-// Color levels
-constexpr std::array<short, NUM_LEVELS> LEVELS = {0, 200, 400, 600, 800, 1000};
-
-// Offset to avoid overwriting ncurses default colors
-constexpr short COLOR_ENCODING_OFFSET = 8;
-// Offset to avoid overwriting ncurses default color pair
-constexpr short PAIR_ENCODING_OFFSET = 1;
-
-}  // namespace
 
 
 namespace raster
@@ -32,42 +13,21 @@ namespace raster
  * 216 colors. Thus, a color is encoded as an integer according to the formula: `R * 36 + G * 6 + B`. The specific
  * palette is given in `LEVELS`.
  */
-inline void init_colors()
-{
-    start_color();
-
-    // define colors
-    for (int r = 0; r < NUM_LEVELS; ++r) {
-        for (int g = 0; g < NUM_LEVELS; ++g) {
-            for (int b = 0; b < NUM_LEVELS; ++b) {
-                const short encoding = (r * NUM_LEVELS + g) * NUM_LEVELS + b;
-                const short color_encoding = encoding + COLOR_ENCODING_OFFSET;
-                const short pair_encoding = encoding + PAIR_ENCODING_OFFSET;
-                init_color(color_encoding, LEVELS[r], LEVELS[g], LEVELS[b]);
-                init_pair(pair_encoding, color_encoding, color_encoding);
-            }
-        }
-    }
-}
-
-/**
- * Convert color value in [0, 1] to the corresponding level, i.e. index in `LEVELS`.
- */
-inline int color_to_level(float color)
-{
-    return std::clamp(static_cast<int>(std::floor(color * NUM_LEVELS)), 0, NUM_LEVELS - 1);
-}
-
+void init_colors();
 
 /**
  * Convert RGB value normalized to [0, 1] to the closest ncurses color pair.
  */
-inline short rgb_to_color_pair(const Eigen::Vector3f& color)
-{
-    const int r = color_to_level(color(0));
-    const int g = color_to_level(color(1));
-    const int b = color_to_level(color(2));
-    return (r * NUM_LEVELS + g) * NUM_LEVELS + b + PAIR_ENCODING_OFFSET;
-}
+short rgb_to_color_pair(const Eigen::Vector3f& color);
+
+/**
+ * Convert from sRGB color space to linear color space.
+ */
+Eigen::Vector3f srgb_to_linear(const Eigen::Vector3f& srgb);
+
+/**
+ * Convert from linear color space to sRGB color space.
+ */
+Eigen::Vector3f linear_to_srgb(const Eigen::Vector3f& linear);
 
 }  // namespace raster
